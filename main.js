4d649dae7b453cb6582ca7c299d21a235612c67b
Oliviarasyya <oliviarasya8@gmail.com>
2025-08-03 12:49:15 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+    strings:["Canva Designer", "CDR and Adobe Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
+
